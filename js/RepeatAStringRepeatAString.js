73e8d877896a671f2096193cdd89f7a694946ca6
Paul Standley <paulstandley1972@gmail.com>
2019-03-14 14:26:25 +0000

    Repeat a String Repeat a String

--- js/RepeatAStringRepeatAString.js
+++ js/RepeatAStringRepeatAString.js
@@ -4,7 +4,19 @@ Repeat a given string str (first argument) for num times (second argument). Retu
 */
 function repeatStringNumTimes(str, num) {
   // repeat after me
-  return str;
+// check for negitive numbers return empty string
+  if(num >= 0) {
+// make an empty string return variable    
+    let retstr = "";
+// and string together num times then return    
+    for(let i = 1; i < num; i++) {
+      retstr += str;
+    }
+    document.write(str,  retstr)
+    return retstr;
+  }else{
+    return "";
+  }
 }
 
 repeatStringNumTimes("abc", 3);// "abcabcabc"

425b1d1efa447d1ae59c9a75cca4cda04f42529b
Paul Standley <paulstandley1972@gmail.com>
2019-03-26 20:57:01 +0000

    Chunky Monkey

--- js/basic/ChunkyMonkey.js
+++ js/basic/ChunkyMonkey.js
@@ -5,12 +5,27 @@ Write a function that splits an array (first argument) into groups the length of
 
 function chunkArrayInGroups(arr, size) {
   // Break it up.
-  return arr;
+// make variables  
+  let slice_holder = "";
+  let retVal = [];
+  let num = 0;
+  let num_size = size;
+// iterate over the array by dividing the array length over size and rounding up  
+  for(let i = 0; i <= Math.ceil(arr.length / size) - 1; i++) {
+// slice the array at num untill num_size    
+    slice_holder = arr.slice(num, num_size);
+// add size to to them and push slice holder to return value empty slice_holder    
+    num += size;
+    num_size += size;
+    retVal.push(slice_holder);
+    slice_holder = "";
+  }
+  return retVal;
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);// should return [["a", "b"], ["c", "d"]].
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);// should return [[0, 1, 2], [3, 4, 5]].
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);// should return [[0, 1], [2, 3], [4, 5]].
+//chunkArrayInGroups(["a", "b", "c", "d"], 2);// should return [["a", "b"], ["c", "d"]].
+//chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3);// should return [[0, 1, 2], [3, 4, 5]].
+//chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2);// should return [[0, 1], [2, 3], [4, 5]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4);// should return [[0, 1, 2, 3], [4, 5]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3);// should return [[0, 1, 2], [3, 4, 5], [6]].
 chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4);// should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].

68bae790465e3b1fbc13c917870b0b879508195b
Paul Standley <paulstandley1972@gmail.com>
2019-03-28 00:08:31 +0000

    Intermediate Algorithm Sum All Numbers in a Range

--- js/Intermediate/SumAllNumbersInARange.js
+++ js/Intermediate/SumAllNumbersInARange.js
@@ -6,12 +6,28 @@ The lowest number will not always come first.
 */
 
 function sumAll(arr) {
-  
-  return 1;
+// sort array to make sure the lowest values is at first index  
+  let array = arr.sort(function(a, b) {
+    return a - b;
+  });
+// add values put it in sum, find the distances between them, get the start and end values
+  const sum = array[0] + array[1],between = array[1] - array[0] - 1,start = array[0] + 1;
+// loop over to keep track (2 + 3 = 5 retVal) (5 + 10 = 15 retVal)
+  let current = start;
+  let retVal = 0;
+  for(let i = 0; i < between; i++) {
+    retVal += current;
+    current++;
+  }
+/* add sum to retVal then return value
+     sum     +   retVal  
+(1 + 4 = 5) + (2 + 3 = 5) = 10 
+(5 + 10 = 15) + (6 + 7 + 8 + 9 = 30) = 45 */
+  retVal += sum; 
+  return retVal;
 }
 
-sumAll([1, 4]);// should return a number.
-sumAll([1, 4]);// should return 10.
-sumAll([4, 1]);// should return 10.
-sumAll([5, 10]);// should return 45.
-sumAll([10, 5]);// should return 45.
+sumAll([1, 4]);// should return 10. (1 + 4 = 5) + (2 + 3 = 5) = 10
+sumAll([4, 1]);// should return 10.  
+sumAll([5, 10]);// should return 45. (5 + 10 = 15) + (6 + 7 + 8 + 9 = 30) = 45
+sumAll([10, 5]);// should return 45. 
